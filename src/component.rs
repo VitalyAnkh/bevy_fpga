f6b2440dd8bd6ab3cf9dc20385c20e1510af6319
vitalyr <vitalyankh@gmail.com>
2022-09-05 14:32:47 +0800

    Initial commit

---
+++ src/component.rs
@@ -0,0 +1,14 @@
+enum Interface {
+    InputType = 1,
+    OutputType = 2,
+}
+
+enum ComponentType {
+    ButtonType = 1,
+    SwitchType,
+    NaiveLedType,
+    RgbLedType,
+    Segs7Type,
+    VgaType,
+    KeyboardType,
+}
---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+mod component;
+mod keyboard;
+mod vga;
+mod render;
+mod at_scancode;
+mod config;
